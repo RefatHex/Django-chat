e3b352873df4112cc04e1a63ddaf44cb2d3652a0
refat <khondakar00@gmail.com>
2023-12-13 22:52:48 -0700

    room creation functionality  added

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('',views.home , name='home')
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
+
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from chat.models import Room, Message
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
+
+def room(request, room):
+    return render(request, 'room.html')
+
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
