fdcd255c35a5b1f1bff195b2d0a0c2562cd0abde
refat <khondakar00@gmail.com>
2023-12-13 21:54:07 -0700

    redirected main page to home and added models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Room(models.Model):
+    name=models.CharField(max_length=255)
+
+class Message(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField(default= datetime.now,blank=True)
+    username=models.CharField(max_length=255)
+    room_name=models.CharField(max_length=255)
+
